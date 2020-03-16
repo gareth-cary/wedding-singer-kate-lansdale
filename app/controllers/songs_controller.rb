44c56740bbbed995a72d94efb1c14cdcd69cbbfa
Gareth Cary <gareth@caryfamily.co.uk>
2020-03-16 01:12:21 +0000

    style wedsite

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,16 @@
 class SongsController < ApplicationController
 
-  def index
-    @songs = Song.all
+  def list
+    @songs = Song.where(artist: params.dig(:search, :artist))
+    @songs = Song.all if @songs.empty?
+  end
+
+
+  def show
+    @song = Song.find(params[:id])
   end
 
   def new
     @song = Song.new # needed to instantiate the form_for
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+
+puts "creating...."
+Song.create(name: 'Amazing Grace',
+  artist: 'American')
+
+Song.create(name: 'Im Forever Blowing Bubbles',
+  artist: 'American')
+
+Song.create(name: 'Clementine',
+  artist: 'American')
+
+Song.create(name: 'Wade in the Water',
+  artist: 'American')
+
+Song.create(name: 'Wayfaring Stranger',
+  artist: 'American')
+
+Song.create(name: 'The Little Turtle Dove',
+  artist: 'American')
+
+Song.create(name: 'Tiree Love Song',
+  artist: 'Irish')
+
+Song.create(name: 'Carrickfergus',
+  artist: 'Irish')
+
+Song.create(name: 'Danny Boy',
+  artist: 'Irish')
+
+Song.create(name: 'She Moved Through the Fair',
+  artist: 'Irish')
+
+Song.create(name: 'The Parting Glass',
+  artist: 'Irish/Scottish')
+
+Song.create(name: 'Banks of the Roses',
+  artist: 'Irish')
+
+Song.create(name: 'Rose of Tralee',
+  artist: 'Irish')
+
+Song.create(name: 'In the Wheatfield',
+  artist: 'Welsh')
+
+Song.create(name: 'The Ash Grove',
+  artist: 'Welsh')
+puts "done...."
