a802429c6fee2fb15b524e7800c7c3c4240243c7
Gareth Cary <gareth@caryfamily.co.uk>
2020-03-19 21:15:31 +0000

    style

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_155303) do
+ActiveRecord::Schema.define(version: 2020_03_19_195027) do
 
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.string "nickname"
   end
 
   create_table "songs", force: :cascade do |t|
