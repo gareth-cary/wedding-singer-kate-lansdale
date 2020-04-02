8c61672207773b8428b43016d036f2ca72ce7ca8
Gareth Cary <gareth@caryfamily.co.uk>
2020-04-02 18:35:36 +0100

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,47 +9,95 @@ Song.destroy_all
 
 puts "creating...."
 Song.create(name: 'Amazing Grace',
-  artist: 'American')
+  artist: 'American',
+  image: 'https://images.unsplash.com/photo-1547589857-fc93330aa0e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80')
 
 Song.create(name: 'Im Forever Blowing Bubbles',
-  artist: 'American')
+  artist: 'American',
+  image: 'https://images.unsplash.com/photo-1547589857-fc93330aa0e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80')
 
 Song.create(name: 'Clementine',
-  artist: 'American')
+  artist: 'American',
+  image: 'https://images.unsplash.com/photo-1547589857-fc93330aa0e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80')
 
 Song.create(name: 'Wade in the Water',
-  artist: 'American')
+  artist: 'American',
+  image: 'https://images.unsplash.com/photo-1547589857-fc93330aa0e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80')
 
 Song.create(name: 'Wayfaring Stranger',
-  artist: 'American')
+  artist: 'American',
+  image: 'https://images.unsplash.com/photo-1547589857-fc93330aa0e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80')
 
 Song.create(name: 'The Little Turtle Dove',
-  artist: 'American')
+  artist: 'American',
+  image: 'https://images.unsplash.com/photo-1547589857-fc93330aa0e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80')
 
 Song.create(name: 'Tiree Love Song',
-  artist: 'Irish')
+  artist: 'Irish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'Carrickfergus',
-  artist: 'Irish')
+  artist: 'Irish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'Danny Boy',
-  artist: 'Irish')
+  artist: 'Irish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'She Moved Through the Fair',
-  artist: 'Irish')
+  artist: 'Irish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'The Parting Glass',
-  artist: 'Irish/Scottish')
+  artist: 'Irish/Scottish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'Banks of the Roses',
-  artist: 'Irish')
+  artist: 'Irish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'Rose of Tralee',
-  artist: 'Irish')
+  artist: 'Irish',
+  image: 'https://images.unsplash.com/photo-1545066230-54c1edc1cdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1535&q=80')
 
 Song.create(name: 'In the Wheatfield',
-  artist: 'Welsh')
+  artist: 'Welsh',
+  image: 'https://images.unsplash.com/photo-1523257813706-ff6e594e4880?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
 
 Song.create(name: 'The Ash Grove',
-  artist: 'Welsh')
+  artist: 'Welsh',
+  image: 'https://images.unsplash.com/photo-1523257813706-ff6e594e4880?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+
+Song.create(name: 'Make You Feel Free',
+  artist: 'Adele',
+  image: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+
+Song.create(name: 'Valerie',
+  artist: 'Amy Winehouse',
+  image: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+
+Song.create(name: 'Moon River',
+  artist: 'Audrey Hepburn',
+  image: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+
+Song.create(name: 'In My Life',
+  artist: 'The Beatles',
+  image: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+
+Song.create(name: 'The Rose',
+  artist: 'Bette Middler',
+  image: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+
 puts "done...."
+
+
+
+
+
+
+
+
+
+
+
+
