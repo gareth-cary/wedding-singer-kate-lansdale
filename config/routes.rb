aca765a9c959ea9efb934a400d9112f91dcb2718
Gareth Cary <gareth@caryfamily.co.uk>
2020-05-06 22:16:03 +0100

    added sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources "contacts", only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
@@ -9,5 +10,4 @@ Rails.application.routes.draw do
   get "songs/list", to: "songs#list"
   get "gallery", to: "pages#gallery"
 
-  resources "contacts", only: [:new, :create]
 end
